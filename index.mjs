b11e6a4bb27eb37ccbeacd08fbe2053e8ae8c6c5
gsmjyw <zetafie125@gmail.com>
2022-10-13 23:58:22 +0900

    feat: 디스코드 로그인 구현, ssl 인증서 추가

--- index.mjs
+++ index.mjs
@@ -1,11 +1,19 @@
-const express = require('express')
-const bodyParser = require('body-parser')
-const mysql = require('mysql2/promise')
+import express from 'express'
+import { fileURLToPath } from 'url'
+import { dirname } from 'path'
+import fs, { accessSync } from 'fs'
+import http from 'http'
+import https from 'https'
+import bodyParser from 'body-parser'
+import mysql from 'mysql2/promise'
+import DiscordOauth2 from 'discord-oauth2'
+import cookieParser from 'cookie-parser'
+import Crypto from 'crypto'
 
 const args = process.argv.slice(2)
 
-if (args.length < 4) {
-  console.error('Parameters not provided: [host] [user] [password] [database]')
+if (args.length < 6) {
+  console.error('Parameters not provided: [host] [user] [password] [database] [discord_oauth_client_secret] [discord_oauth_redirect_uri]')
   process.exit(1)
 }
 
@@ -20,10 +28,80 @@ const pool = mysql.createPool({
 const app = express();
 
 app.use(bodyParser.json())
+app.use(cookieParser())
 app.use(express.static('public'))
 
+const __filename = fileURLToPath(import.meta.url)
+const __dirname = dirname(__filename)
+
+const key = fs.readFileSync(`${__dirname}/ssl/${fs.readdirSync('ssl').find((file) => file.endsWith('.key.pem'))}`)
+const cert = fs.readFileSync(`${__dirname}/ssl/${fs.readdirSync('ssl').find((file) => file.endsWith('.crt.pem'))}`)
+
+const oauth = new DiscordOauth2({
+  clientId: '1029472862765056010',
+  clientSecret: args[4],
+  redirectUri: args[5],
+})
+
+app.use((req, res, next) => {
+  const protocol = req.headers['x-forwarded-proto'] || req.protocol
+  if (protocol == 'https') {
+    next()
+  }
+  else {
+    res.redirect(`https://${req.hostname}${req.url}`)
+  }
+})
+
 app.get('/', (req, res) => {
-  res.sendFile(__dirname + '/views/track_select.html');
+  res.sendFile(__dirname + '/views/lobby.html')
+})
+
+app.get('/discord/oauth', async (req, res) => {
+  try {
+    const code = req.query.code
+
+    if (!code) {
+      throw new Error('authorization code not provided')
+    }
+
+    let resp = await oauth.tokenRequest({
+      code: code,
+      scope: 'identify',
+      grantType: 'authorization_code',
+    })
+
+    if (!resp.access_token) {
+      throw new Error('invalid authorization code')
+    }
+
+    const encrypted = Crypto.publicEncrypt(key, Buffer.from(resp.access_token)).toString('base64')
+    res.cookie('access_token', encrypted)
+  }
+  catch (error) {
+    console.log(error)
+  }
+  finally {
+    res.redirect('/')
+  }
+})
+
+app.post('/discord/user', async (req, res) => {
+  try {
+    const accessToken = Crypto.privateDecrypt(key, Buffer.from(decodeURIComponent(req.body.access_token), 'base64'))
+    const user = await oauth.getUser(accessToken)
+
+    res.json({
+      result: 'OK',
+      user: user,
+    })
+  }
+  catch (error) {
+    res.json({
+      result: 'error',
+      error: error,
+    })
+  }
 })
 
 app.post('/trackAmount', async (req, res) => {
@@ -70,4 +148,5 @@ app.post('/trackAmount', async (req, res) => {
   }
 })
 
-app.listen(80);
+http.createServer(app).listen(80)
+https.createServer({ key: key, cert: cert }, app).listen(443)
