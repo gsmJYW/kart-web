3e5829c6bd219d338c6d2a933ba7daae02ad00f3
gsmjyw <zetafie125@gmail.com>
2022-10-19 01:41:03 +0900

    feat: sql 세션 추가, 게임 생성 및 감가 추가, 로그아웃 추가

--- index.mjs
+++ index.mjs
@@ -1,35 +1,49 @@
 import express from 'express'
 import { fileURLToPath } from 'url'
 import { dirname } from 'path'
-import fs, { accessSync } from 'fs'
+import fs from 'fs'
 import http from 'http'
 import https from 'https'
 import bodyParser from 'body-parser'
 import mysql from 'mysql2/promise'
 import DiscordOauth2 from 'discord-oauth2'
-import cookieParser from 'cookie-parser'
 import Crypto from 'crypto'
+import session from 'express-session'
+import mysqlSession from 'express-mysql-session'
+import fetch from 'node-fetch'
 
 const args = process.argv.slice(2)
 
 if (args.length < 6) {
-  console.error('Parameters not provided: [host] [user] [password] [database] [discord_oauth_client_secret] [discord_oauth_redirect_uri]')
+  console.error('Parameters not provided: [mysql_host] [mysql_user] [mysql_password] [mysql_database] [session_secret] [discord_oauth_client_secret] [discord_oauth_redirect_uri] [kart_api_key]')
   process.exit(1)
 }
 
-const pool = mysql.createPool({
+const options = {
   host: args[0],
   user: args[1],
   password: args[2],
   database: args[3],
   connectionLimit: 100,
-})
+}
 
-const app = express();
+const pool = mysql.createPool(options)
+await pool.query(`ALTER USER '${args[1]}'@'localhost' IDENTIFIED WITH mysql_native_password BY '${args[2]}'`)
+await pool.query('DELETE FROM game WHERE closed_at IS NULL')
+
+const mysqlStore = mysqlSession(session)
+const sessionStore = new mysqlStore(options)
+
+const app = express()
 
 app.use(bodyParser.json())
-app.use(cookieParser())
 app.use(express.static('public'))
+app.use(session({
+  secret: args[4],
+  resave: false,
+  saveUninitialized: true,
+  store: sessionStore,
+}))
 
 const __filename = fileURLToPath(import.meta.url)
 const __dirname = dirname(__filename)
@@ -37,11 +51,16 @@ const __dirname = dirname(__filename)
 const key = fs.readFileSync(`${__dirname}/ssl/${fs.readdirSync('ssl').find((file) => file.endsWith('.key.pem'))}`)
 const cert = fs.readFileSync(`${__dirname}/ssl/${fs.readdirSync('ssl').find((file) => file.endsWith('.crt.pem'))}`)
 
+http.createServer(app).listen(80)
+https.createServer({ key: key, cert: cert }, app).listen(443)
+
 const oauth = new DiscordOauth2({
   clientId: '1029472862765056010',
-  clientSecret: args[4],
-  redirectUri: args[5],
+  clientSecret: args[5],
+  redirectUri: args[6],
 })
+
+const kartApiKey = args[7]
 
 app.use((req, res, next) => {
   const protocol = req.headers['x-forwarded-proto'] || req.protocol
@@ -53,8 +72,84 @@ app.use((req, res, next) => {
   }
 })
 
-app.get('/', (req, res) => {
-  res.sendFile(__dirname + '/views/lobby.html')
+app.get('/', async (req, res) => {
+  try {
+    if (!req.session.access_token) {
+      throw new Error()
+    }
+
+    const accessToken = decrypt(req.session.access_token)
+    const user = await oauth.getUser(accessToken)
+
+    const result = await pool.query(`SELECT * FROM user WHERE id = ${user.id}`)
+
+    if (result[0][0]) {
+      await pool.query(`UPDATE user SET name = '${user.username}', avatar = '${user.avatar}'`)
+      res.sendFile(__dirname + '/views/lobby.html')
+    }
+    else {
+      res.sendFile(__dirname + '/views/signup.html')
+    }
+  }
+  catch (error) {
+    res.sendFile(__dirname + '/views/signin.html')
+  }
+})
+
+app.post('/signup', async (req, res) => {
+  try {
+    if (!req.session.access_token) {
+      throw new Error('not authorized')
+    }
+
+    const result = await fetch(`https://api.nexon.co.kr/kart/v1.0/users/nickname/${req.body.rider_name}`, {
+      headers: {
+        Authorization: kartApiKey,
+      },
+    })
+
+    const rider = await result.json()
+
+    if (!rider.accessId) {
+      throw new Error('라이더를 찾지 못했습니다.')
+    }
+
+    const accessToken = decrypt(req.session.access_token)
+    const user = await oauth.getUser(accessToken)
+
+    await pool.query(`INSERT INTO user (id, name, avatar, rider_id) VALUES (${user.id}, '${user.username}', '${user.avatar}', '${rider.accessId}')`)
+
+    res.json({
+      result: 'OK',
+    })
+  }
+  catch (error) {
+    res.json({
+      result: 'error',
+      error: error.message,
+    })
+  }
+})
+
+app.get('/user', async (req, res) => {
+  try {
+    if (!req.session.access_token) {
+      throw new Error('not authorized')
+    }
+
+    const result = await pool.query(`SELECT * FROM user WHERE id = ${decrypt(req.session.user_id)}`)
+
+    res.json({
+      result: 'OK',
+      user: result[0][0],
+    })
+  }
+  catch (error) {
+    res.json({
+      result: 'error',
+      error: error.message,
+    })
+  }
 })
 
 app.get('/discord/oauth', async (req, res) => {
@@ -75,78 +170,240 @@ app.get('/discord/oauth', async (req, res) => {
       throw new Error('invalid authorization code')
     }
 
-    const encrypted = Crypto.publicEncrypt(key, Buffer.from(resp.access_token)).toString('base64')
-    res.cookie('access_token', encrypted)
-  }
-  catch (error) {
-    console.log(error)
+    const encryptedToken = encrypt(resp.access_token)
+
+    const user = await oauth.getUser(resp.access_token)
+    const encryptedId = encrypt(user.id)
+
+    req.session.access_token = encryptedToken
+    req.session.user_id = encryptedId
+    req.session.save()
   }
   finally {
     res.redirect('/')
   }
 })
 
-app.post('/discord/user', async (req, res) => {
+app.get('/signout', async (req, res) => {
+  delete req.session.access_token
+  delete req.session.user_id
+  req.session.save()
+
+  res.redirect('/')
+})
+
+app.post('/tracks', async (req, res) => {
   try {
-    const accessToken = Crypto.privateDecrypt(key, Buffer.from(decodeURIComponent(req.body.access_token), 'base64'))
-    const user = await oauth.getUser(accessToken)
+    if (!req.session.access_token) {
+      throw new Error('not authorized')
+    }
+
+    const trackList = await getTrackList(req.body.mode, req.body.track_type)
 
     res.json({
       result: 'OK',
-      user: user,
+      tracks: trackList,
     })
   }
   catch (error) {
     res.json({
       result: 'error',
-      error: error,
+      error: error.message,
     })
   }
 })
 
-app.post('/trackAmount', async (req, res) => {
-  try {
-    let trackType = req.body.track_type
-    let mode = req.body.mode
+function getTrackList(mode, trackType) {
+  return new Promise(async (resolve, reject) => {
+    if (!mode || !trackType) {
+      reject(new Error('parameters required'))
+    }
 
-    if (!trackType && !mode) {
-      throw new Error('parameters required')
+    if (!['speed', 'item'].some((element) => mode == element)) {
+      reject(new Error('invalid mode'))
+    }
+
+    if (!['very_easy', 'easy', 'normal', 'hard', 'very_hard', 'all', 'league', 'new', 'reverse', 'crazy'].some((element) => trackType == element)) {
+      reject(new Error('invalid trackType'))
     }
 
     const conditionList = []
 
     if (mode == 'speed') {
-      conditionList.push('crazy = false')
-
-      if (trackType == 'league') {
-        conditionList.push('speed = true')
+      if (trackType == 'crazy') {
+        reject(new Error('speed mode is not available on crazy random'))
       }
+
+      conditionList.push('crazy = false')
     }
     else {
       conditionList.push('item = true')
     }
 
-    if (['very', 'easy', 'normal', 'hard', 'new'].some(element => trackType.includes(element)) > 0) {
+    if (['all', 'reverse', 'crazy'].some((element) => trackType == element) == 0) {
       conditionList.push(`${mode}_${trackType} = true`)
     }
     else if (trackType != 'all') {
       conditionList.push(`${trackType} = true`)
     }
 
-    const result = await pool.query(`SELECT COUNT(*) AS trackAmount FROM track WHERE ${conditionList.join(' AND ')}`)
+    try {
+      const result = await pool.query(`SELECT * FROM track WHERE ${conditionList.join(' AND ')}`)
+      resolve(result[0])
+    }
+    catch (error) {
+      reject(new Error(error))
+    }
+  })
+}
+
+app.post('/game', async (req, res) => {
+  try {
+    if (!req.session.access_token) {
+      throw new Error('not authorized')
+    }
+
+    const userId = decrypt(req.session.user_id)
+    const result = await pool.query(`SELECT * FROM game WHERE '${userId}' IN (host_id, opponent_id) AND closed_at IS NULL`)
+    const game = result[0][0]
 
     res.json({
       result: 'OK',
-      trackAmount: result[0][0].trackAmount,
+      game: game ? game : {},
     })
   }
   catch (error) {
     res.json({
       result: 'error',
-      error: error,
+      error: error.message,
     })
   }
 })
 
-http.createServer(app).listen(80)
-https.createServer({ key: key, cert: cert }, app).listen(443)
+app.post('/game/create', async (req, res) => {
+  try {
+    const mode = req.body.mode
+    const trackType = req.body.track_type
+    const banpickAmount = req.body.banpick_amount
+    const userId = decrypt(req.session.user_id)
+
+    const trackList = await getTrackList(mode, trackType)
+
+    if (banpickAmount < 9 || banpickAmount > trackList.length) {
+      throw new Error(`banpick amount can't be less than 9 or more than track amount`)
+    }
+
+    const conditionList = [`'${userId}' IN (host_id, opponent_id)`]
+
+    if (req.session.player_id) {
+      conditionList.push(`'${decrypt(req.session.player_id)}' IN (host_id, opponent_id)`)
+    }
+
+    let result = await pool.query(`SELECT * FROM game WHERE (${conditionList.join(' OR ')}) AND closed_at IS NULL`)
+
+    if (result[0][0]) {
+      throw new Error('이미 진행 중이신 게임이 있습니다.')
+    }
+
+    result[0].affectedRows = 0
+    let gameId
+
+    req.session.player_id = req.session.user_id
+    req.session.save()
+
+    while (result[0].affectedRows == 0) {
+      gameId = Crypto.randomUUID().slice(0, 6)
+      result = await pool.query(`INSERT IGNORE INTO game (id, host_id, host_rider_id, mode, track_type, banpick_amount) VALUES ('${gameId}', '${userId}', (SELECT rider_id FROM user WHERE id = '${userId}'), '${mode}', '${trackType}', ${banpickAmount})`)
+    }
+
+    res.json({
+      result: 'OK',
+      game_id: gameId,
+    })
+  }
+  catch (error) {
+    res.json({
+      result: 'error',
+      error: error.message,
+    })
+  }
+})
+
+app.post('/game/join', async (req, res) => {
+  try {
+    const gameId = req.body.game_id
+    const userId = decrypt(req.session.user_id)
+
+    const conditionList = [`'${userId}' IN (host_id, opponent_id)`]
+
+    if (req.session.player_id) {
+      conditionList.push(`'${decrypt(req.session.player_id)}' IN (host_id, opponent_id)`)
+    }
+
+    let result = await pool.query(`SELECT * FROM game WHERE (${conditionList.join(' OR ')}) AND closed_at IS NULL`)
+
+    if (result[0][0]) {
+      throw new Error('이미 진행 중이신 게임이 있습니다.')
+    }
+
+    result = await pool.query(`SELECT * FROM game WHERE id = '${gameId}'`)
+    const game = result[0][0]
+
+    if (!game) {
+      throw new Error('존재하지 않는 초대 코드입니다.')
+    }
+    else if (game.opponent_id || game.closed_at) {
+      throw new Error('만료된 초대 코드입니다.')
+    }
+
+    result = await pool.query(`SELECT * FROM user WHERE id = ${userId}`)
+
+    if (game.host_rider_id == result[0][0].rider_id) {
+      throw new Error('호스트와 라이더명이 같습니다. <br> 라이더명을 변경해주세요.')
+    }
+
+    req.session.player_id = req.session.user_id
+    req.session.save()
+
+    await pool.query(`UPDATE game SET opponent_id = ${userId} WHERE id = ${gameId}`)
+
+    res.json({
+      result: 'OK',
+    })
+  }
+  catch (error) {
+    res.json({
+      result: 'error',
+      error: error.message,
+    })
+  }
+})
+
+app.post('/game/close', async (req, res) => {
+  try {
+    if (!req.session.access_token) {
+      throw new Error('not authorized')
+    }
+
+    const userId = decrypt(req.session.user_id)
+    await pool.query(`DELETE FROM game WHERE '${userId}' IN (host_id, opponent_id) AND closed_at IS NULL`)
+
+    res.json({
+      result: 'OK',
+    })
+  }
+  catch (error) {
+    res.json({
+      result: 'error',
+      error: error.message,
+    })
+  }
+})
+
+function encrypt(str) {
+  return Crypto.publicEncrypt(key, Buffer.from(str)).toString('base64')
+}
+
+function decrypt(str) {
+  return Crypto.privateDecrypt(key, Buffer.from(str, 'base64')).toString()
+}
--- public/scripts/lobby.mjs
+++ public/scripts/lobby.mjs
@@ -3,7 +3,6 @@ function postAsync(url, params = {}) {
         fetch(url, {
             method: 'POST',
             headers: {
-                'Accept': 'application/json',
                 'Content-Type': 'application/json',
             },
             body: JSON.stringify(params),
@@ -13,47 +12,173 @@ function postAsync(url, params = {}) {
     })
 }
 
-async function signinAlert() {
-    await Swal.fire({
-        icon: 'success',
-        title: '환영합니다',
-        html: '서비스를 이용하기 위해 Discord 로그인이 필요합니다.<br>원치 않으실 경우 사이트를 닫아주세요.',
-        allowOutsideClick: false,
-        confirmButtonText: '확인',
-    })
+function buildGameCard(gameId, mode, trackType, banpickAmount) {
+    let title
 
-    window.location = `https://discord.com/api/oauth2/authorize?client_id=1029472862765056010&redirect_uri=${document.URL}discord/oauth&response_type=code&scope=identify`
-}
+    switch (mode) {
+        case 'speed':
+            title = '스피드'
+            break
 
-const cookie = Object.fromEntries(document.cookie.split(/; */).map(cookie => cookie.split('=', 2)))
-let user
-
-if (cookie.access_token) {
-    const res = await postAsync('/discord/user', { access_token: cookie.access_token })
-
-    if (res.result != 'OK') {
-        await signinAlert()
+        case 'item':
+            title = '아이템'
+            break
     }
 
-    user = res.user
-}
-else {
-    await signinAlert()
+    title += ' / '
+
+    switch (trackType) {
+        case 'very_easy':
+            title += 'Very Easy 랜덤'
+            break
+
+        case 'easy':
+            title += 'Easy 랜덤'
+            break
+
+        case 'normal':
+            title += 'Normal 랜덤'
+            break
+
+        case 'hard':
+            title += 'Hard 랜덤'
+            break
+
+        case 'very_hard':
+            title += 'Very Hard 랜덤'
+            break
+
+        case 'all':
+            title += '전체 랜덤'
+            break
+
+        case 'league':
+            title += '리그 랜덤'
+            break
+
+        case 'new':
+            title += '뉴 랜덤'
+            break
+
+        case 'reverse':
+            title += '리버스 랜덤'
+            break
+
+        case 'crazy':
+            title += '크레이지 랜덤'
+            break
+    }
+
+    document.querySelector('#game-card-title').textContent = title
+    document.querySelector('#banpick-amount').textContent = banpickAmount
+    document.querySelector('#game-card').hidden = false
+    document.querySelector('#show-game-id').addEventListener('click', async () => {
+        await Swal.fire({
+            title: `초대 코드`,
+            html: gameId,
+            confirmButtonText: '확인',
+        })
+    })
+    document.querySelector('#close-game').addEventListener('click', async () => {
+        try {
+            const res = await postAsync('/game/close')
+
+            if (res.result == 'error') {
+                throw new Error(res.error)
+            }
+
+            document.querySelector('#game-card').hidden = true
+        }
+        catch (error) {
+            await Swal.fire({
+                icon: 'warning',
+                html: error.message,
+                confirmButtonText: '확인',
+            })
+        }
+    })
 }
 
-document.querySelector('#signout').addEventListener('click', () => {
-    document.cookie = "access_token=; expires=Thu, 01 Jan 1970 00:00:00 UTC; path=/;";
-    location.reload()
+try {
+    const res = await postAsync('/game')
+
+    if (res.result == 'error') {
+        throw new Error(res.error)
+    }
+
+    if (res.game.id) {
+        buildGameCard(res.game.id, res.game.mode, res.game.track_type, res.game.banpick_amount)
+    }
+}
+catch (error) {
+    await Swal.fire({
+        icon: 'warning',
+        html: error.message,
+        confirmButtonText: '확인',
+    })
+}
+
+document.querySelector('#signout').addEventListener('click', async () => {
+    const res = await Swal.fire({
+        icon: 'warning',
+        html: '로그아웃 하여도 대기 중이거나 <br> 진행 중인 게임은 취소되지 않습니다.',
+        showCancelButton: true,
+        confirmButtonText: '로그아웃',
+        cancelButtonText: '취소'
+    })
+
+    if (res.isConfirmed) {
+        await fetch('/signout')
+        location.reload()
+    }
 })
 
-for (const random of document.querySelectorAll('.random-list > img')) {
+document.querySelector('#join').addEventListener('click', async () => {
+    const res = await Swal.fire({
+        title: '초대 코드 입력',
+        input: 'text',
+        showCancelButton: true,
+        confirmButtonText: '확인',
+        cancelButtonText: '취소',
+    })
+
+    if (res.isConfirmed) {
+        const gameId = res.value.trim()
+
+        if (/[^A-Za-z0-9]/g.test(gameId) || gameId.length != 6) {
+            await Swal.fire({
+                icon: 'warning',
+                html: '초대 코드에 잘못된 문자가 들어갔거나 6자가 아닙니다.',
+                confirmButtonText: '확인',
+            })
+            return
+        }
+
+        try {
+            const res = await postAsync('/game/join', { game_id: gameId })
+
+            if (res.result == 'error') {
+                throw new Error(res.error)
+            }
+        }
+        catch (error) {
+            await Swal.fire({
+                icon: 'warning',
+                html: error.message,
+                confirmButtonText: '확인',
+            })
+        }
+    }
+})
+
+for (const random of document.querySelectorAll('.track-type-list > img')) {
     random.addEventListener('click', async (e) => {
-        const randomType = e.target.id
+        const trackType = e.target.id
         let mode = 'item'
 
-        if (randomType != 'crazy') {
+        if (trackType != 'crazy') {
             const res = await Swal.fire({
-                html: `<img src="/images/${randomType}.png" />`,
+                html: `<img src="/images/${trackType}.png" />`,
                 input: 'radio',
                 inputOptions: {
                     'item': '아이템전',
@@ -72,18 +197,21 @@ for (const random of document.querySelectorAll('.random-list > img')) {
             mode = res.value
         }
 
+        let banpickAmount = 0
+
         if (mode) {
             try {
-                let res = await postAsync('/trackAmount', { track_type: randomType, mode: mode })
+                let res = await postAsync('/tracks', { track_type: trackType, mode: mode })
 
                 if (res.result == 'error') {
                     throw new Error(res.error)
                 }
 
-                if (res.trackAmount < 9) {
+                if (res.tracks.length < 9) {
                     await Swal.fire({
                         icon: 'warning',
-                        html: '트랙 수가 밴픽을 진행하기에 부족합니다.'
+                        html: '트랙 수가 밴픽을 진행하기에 부족합니다.',
+                        confirmButtonText: '확인',
                     })
                     return
                 }
@@ -92,24 +220,64 @@ for (const random of document.querySelectorAll('.random-list > img')) {
                     title: '밴픽 트랙 수',
                     icon: 'question',
                     input: 'range',
-                    inputLabel: (randomType == 'crazy' ? '주의: 크레이지는 아이템전만 가능합니다.\n' : '') + '아래 만큼의 트랙을 랜덤으로 뽑아 밴픽을 진행합니다.',
+                    inputLabel: (trackType == 'crazy' ? '주의: 크레이지는 아이템전만 가능합니다.\n' : '') + '아래 만큼의 트랙을 랜덤으로 뽑아 밴픽을 진행합니다.',
                     inputAttributes: {
                         min: 9,
-                        max: res.trackAmount,
+                        max: res.tracks.length,
                         step: 1
                     },
-                    inputValue: Math.round(9 + (res.trackAmount - 9) / 2),
+                    inputValue: Math.round(9 + (res.tracks.length - 9) / 2),
                     showCancelButton: true,
                     confirmButtonText: '확인',
                     cancelButtonText: '취소',
                 })
 
-                const banpickAmount = res.value
+                if (res.isConfirmed) {
+                    banpickAmount = res.value
+                }
             }
             catch (error) {
                 await Swal.fire({
-                    icon: 'error',
-                    html: `트랙 수를 받아오는 도중 오류가 발생 하였습니다. <br> ${error}`,
+                    icon: 'warning',
+                    html: error.message,
+                    confirmButtonText: '확인',
+                })
+            }
+        }
+
+        if (banpickAmount) {
+            try {
+                swal.fire({
+                    html: '게임을 여는 중입니다.',
+                    showConfirmButton: false,
+                    allowOutsideClick: false,
+                    heightAuto: false,
+                })
+                swal.showLoading()
+
+                let res = await postAsync('/game/create', {
+                    mode: mode,
+                    track_type: trackType,
+                    banpick_amount: banpickAmount,
+                })
+
+                if (res.result == 'error') {
+                    throw new Error(res.error)
+                }
+
+                buildGameCard(res.game_id, mode, trackType, banpickAmount)
+
+                await Swal.fire({
+                    title: `게임 생성 완료`,
+                    icon: 'success',
+                    html: `초대 코드: <strong>${res.game_id}</strong> <br> 사이트에서 나가셔도 취소되지 않습니다.`,
+                    confirmButtonText: '확인',
+                })
+            }
+            catch (error) {
+                await Swal.fire({
+                    icon: 'warning',
+                    html: error.message,
                     confirmButtonText: '확인',
                 })
             }
